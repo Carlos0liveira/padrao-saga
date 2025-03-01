b623fafb94f0c462bea406ad7de5c2a814d0e670
Carlos JosÃ© de Oliveira Junior <oliveiracarlos2001@hotmail.com>
2025-03-01 12:02:13 -0300

    Criando EventController

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
@@ -3,6 +3,7 @@ package br.com.microservices.orchestrated.orderservice.core.consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import br.com.microservices.orchestrated.orderservice.core.service.EventService;
 import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 public class EventConsumer {
 
     private final JsonUtil jsonUtil;
+    private final EventService eventService;
 
     @KafkaListener(
             groupId = "${spring.kafka.consumer.group-id}",
@@ -21,10 +23,7 @@ public class EventConsumer {
     public void consumeNotifyEndingEvent(String payloud) {
         log.info("Receiving ending notification event {} from notify-ending topic", payloud);
         var event = jsonUtil.toEvent(payloud);
-
-        log.info(event.toString());
-
-
+        eventService.notifyEnding(event);
     }
 
 
--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -1,20 +1,67 @@
 package br.com.microservices.orchestrated.orderservice.core.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 import org.springframework.stereotype.Service;
 
+import br.com.microservices.orchestrated.orderservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import br.com.microservices.orchestrated.orderservice.core.dto.EventFilters;
 import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
+import jakarta.servlet.Filter;
+import jakarta.validation.Validation;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class EventService {
 
     private final EventRepository eventRepository;
 
-
     public Event save(Event event) {
         return eventRepository.save(event);
+    }
+
+    public void notifyEnding(Event event) {
+        event.setOrderId(event.getOrderId());
+        event.setCreatedAt(LocalDateTime.now());
+        save(event);
+        log.info("Order {} with saga notified! TransactionID: {}", event.getOrderId(), event.getTransactionId());
+    }
+
+    public List<Event> findAll() {
+        return eventRepository.findAllByOrderByCreatedAtDesc();
+    }
+
+    public Event findByFilters(EventFilters filters) {
+        validateEmptyFilters(filters);
+
+        if (!isEmpty(filters.getOrderId())) {
+            findByOrderId(filters);
+        }
+
+        return findByTransactionId(filters);
+    }
+
+    private Event findByOrderId(EventFilters filters) {
+        return eventRepository.findTop1ByOrderIdOrderByCreatedAtDesc(filters.getOrderId())
+                .orElseThrow(() -> new ValidationException("Event not found by Order ID"));
+    }
+
+    private Event findByTransactionId(EventFilters filters) {
+        return eventRepository.findTop1ByTransactionIdOrderByCreatedAtDesc(filters.getTransactionId())
+                .orElseThrow(() -> new ValidationException("Event not found by Transaction ID"));
+    }
+
+
+    private void validateEmptyFilters(EventFilters filters) {
+        if (isEmpty(filters.getOrderId()) && isEmpty(filters.getTransactionId())) {
+            throw new ValidationException("Order ID or Transaction ID must be informed");
+        }
     }
 
 }
